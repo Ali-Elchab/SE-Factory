35624e118f03592b6c18f8861b1d3a0ecf1e0adf
Ali Hadi Elchab <alielchab01@gmail.com>
2023-11-17 00:02:42 +0200

    Added menu button and tabs functionalities

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,9 @@ let menu = document.querySelector(".menu-container");
 let exitMenu = document.querySelector("#exit-menu");
 let menuLinks = document.querySelectorAll(".link-menu-item");
 
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".tabs-links button:nth-child(1)").click();
+});
 menuButton.addEventListener("click", () => {
   menu.style.cssText = "display:flex;";
 });
@@ -17,3 +20,40 @@ menuLinks.forEach((link) => {
     menu.style.cssText = "display:none;";
   });
 });
+
+function showTab(evt, program) {
+  let tabcontent = document.getElementsByClassName("tabcontent");
+  let currentTab = document.querySelector(`#${program}`);
+  let tablinks = document.getElementsByClassName("tablinks");
+  let programsSection = document.getElementById("programs");
+  let pointer = document.querySelector(`#${program} .fcs-pointer`);
+  let toBeConfirmed = document.querySelector(`#${program} .next-course-styled`);
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  currentTab.style.cssText = "display:flex;";
+  evt.currentTarget.className += " active";
+  console.log(currentTab);
+  if (program == "FSW") {
+    pointer.style.cssText = "color:#28eea7;";
+    programsSection.style.cssText = "background-color:#28eea7;";
+    toBeConfirmed.style.cssText = "color:#28eea7;";
+  } else if (program == "FCS") {
+    pointer.style.cssText = "color: #ffc635;";
+    programsSection.style.cssText = "background-color:#ffc635;";
+    toBeConfirmed.style.cssText = "color:#ffc635;";
+    currentTab.style.cssText = "border-radius:0px 20px 20px 20px;display:flex;";
+  } else if (program == "FSD") {
+    pointer.style.cssText = "color: #9864da;";
+    programsSection.style.cssText = "background-color:#9864da;";
+    toBeConfirmed.style.cssText = "color:#9864da;";
+  } else if (program == "UIX") {
+    pointer.style.cssText = "color: #fb508e;";
+    programsSection.style.cssText = "background-color:#fb508e;";
+    toBeConfirmed.style.cssText = "color:#fb508e;";
+  }
+}
